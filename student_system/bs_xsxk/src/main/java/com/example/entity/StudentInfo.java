727ade07b74d4d4c37f10bb93415c32edbf362d2
wE2for <489384046@qq.com>
2026-05-12 13:35:19 +0800

    根据要求重建仓库项目文件

---
+++ student_system/bs_xsxk/src/main/java/com/example/entity/StudentInfo.java
@@ -0,0 +1,50 @@
+package com.example.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+@Table(name = "student_info")
+public class StudentInfo extends Account {
+
+    @Column(name = "code")
+    private String code;
+    @Column(name = "xueyuanId")
+    private Long xueyuanId;
+    @Column(name = "score")
+    private Integer score;
+    @Transient
+    private String xueyuanName;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Long getXueyuanId() {
+        return xueyuanId;
+    }
+
+    public void setXueyuanId(Long xueyuanId) {
+        this.xueyuanId = xueyuanId;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public String getXueyuanName() {
+        return xueyuanName;
+    }
+
+    public void setXueyuanName(String xueyuanName) {
+        this.xueyuanName = xueyuanName;
+    }
+}
---
+++ student_system/bs_xsxk/src/main/java/com/example/entity/TeacherInfo.java
@@ -0,0 +1,40 @@
+package com.example.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+@Table(name = "teacher_info")
+public class TeacherInfo extends Account {
+
+    @Column(name = "zhicheng")
+    private String zhicheng;
+    @Column(name = "zhuanyeId")
+    private Long zhuanyeId;
+    @Transient
+    private String zhuanyeName;
+
+    public String getZhicheng() {
+        return zhicheng;
+    }
+
+    public void setZhicheng(String zhicheng) {
+        this.zhicheng = zhicheng;
+    }
+
+    public Long getZhuanyeId() {
+        return zhuanyeId;
+    }
+
+    public void setZhuanyeId(Long zhuanyeId) {
+        this.zhuanyeId = zhuanyeId;
+    }
+
+    public String getZhuanyeName() {
+        return zhuanyeName;
+    }
+
+    public void setZhuanyeName(String zhuanyeName) {
+        this.zhuanyeName = zhuanyeName;
+    }
+}
